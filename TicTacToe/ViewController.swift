3d591eb7468130029e427988f9cf697b0a35f3f2
lakhan <lakhan.rathi@kpoint.com>
2019-11-22 12:53:13 +0530

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by kPoint Technologies on 11/22/19.
+//  Copyright © 2019 kPoint Technologies. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        FirebaseApp.configure()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
